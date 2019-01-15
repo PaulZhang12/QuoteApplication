0f747830aa20c16ae943376e78190ee3cb8378b8
Ethan Haezebroeck <ethanHaezebroeck@ethans-mbp.3520.dhcp.asu.edu>
2019-01-14 23:47:50 -0700

    Fully built out bottom nav bar and made ActivityScreen and SearchScreen

--- App.js
+++ App.js
@@ -10,6 +10,10 @@ import React, {Component} from 'react';
 import {createBottomTabNavigator, createAppContainer} from 'react-navigation';
 import ProfileScreen from './src/ProfileScreen.js';
 import FeedScreen from './src/FeedScreen.js';
+import SearchScreen from './src/SearchScreen';
+import ActivityScreen from './src/ActivityScreen';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import MaterialIcon from 'react-native-vector-icons/MaterialIcons'
 
 type Props = {};
 class Profile extends Component<Props> {
@@ -22,10 +26,55 @@ class Feed extends Component<Props> {
     return <FeedScreen />
   }
 }
+class Search extends Component<Props> {
+    render() {
+        return <SearchScreen />
+    }
+}
+
+class Activity extends Component<Props> {
+    render() {
+        return <ActivityScreen />
+    }
+}
 
 const TabNavigator = createBottomTabNavigator({
-    Feed: Feed,
-    Profile: Profile
+    Feed: {
+        screen: Feed,
+        navigationOptions: {
+            tabBarLabel: 'Feed',
+            tabBarIcon: ({tintColor}) => (
+                <FeatherIcon name='home' size={24} />
+            )
+        }
+    },
+    Search: {
+        screen: Search,
+        navigationOptions: {
+            tabBarLabel: 'Search',
+            tabBarIcon: ({tintColor}) => (
+                <FeatherIcon name='search' size={24} />
+            )
+        }
+    },
+    Activity: {
+        screen: Activity,
+        navigationOptions: {
+            tabBarLabel: 'Activity',
+            tabBarIcon: ({tintColor}) => (
+                <FeatherIcon name='inbox' size={24} />
+            )
+        }
+    },
+    Profile: {
+        screen: Profile,
+        navigationOptions: {
+            tabBarLabel: 'Profile',
+            tabBarIcon: ({tintColor}) => (
+                <MaterialIcon name='person-outline' size={24} />
+            )
+        }
+    }
 })
 
 export default createAppContainer(TabNavigator);

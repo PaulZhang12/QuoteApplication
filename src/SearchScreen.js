42023a0beb156143acd71db1ee28c84437f9dfd1
Paul Zhang <PaulZhang12@outlook.com>
2019-01-16 22:35:16 -0600

    Made Profile Screen a lot more comprehensive

--- src/SearchScreen.js
+++ src/SearchScreen.js
@@ -16,6 +16,7 @@ class SearchScreen extends Component {
             <View style={styles.layout}>
                 <View style={styles.searchContainer}>
                     <Icon name="ios-search" size={35} style={styles.searchIcon}/>
+                    <TextInput placeholder="Find Quotes!" style={styles.searchText}></TextInput>
                 </View>
             </View>
         )
@@ -29,16 +30,23 @@ const styles = StyleSheet.create({
     },
 
     searchContainer: {
+        display: "flex",
         width: 90+"%",
         height: 40,
         borderColor: "grey",
         borderWidth: 1,
-        marginTop: 30
+        marginTop: 30,
+        flexDirection: "row"
     },
 
     searchIcon: {
         marginLeft: 10,
         color: "grey"
+    },
+
+    searchText: {
+        fontSize: 18,
+        paddingTop: 6
     }
 
 });

043d62561c15459982b8c90019ab6c256e1ad097
EthanBoss <ethanwboss115@gmail.com>
2019-02-07 22:40:13 -0500

    a

---
+++ src/Components/PostComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { StyleSheet, View, Text, Image} from "react-native";
+import FontIcon from 'react-native-vector-icons/FontAwesome';
+import FeatherIcon from 'react-native-vector-icons/Feather';
+import Ionicon from 'react-native-vector-icons/Ionicons';
+
+import {Card, CardItem, Thumbnail, Body, Left, Right, Button, Icon} from 'native-base';
+
+class PostComponents.js extends Component{
+
+
+
+
+
+}
